959e9c6486479b9a54d647ce0b4c946fbc2fe4c0
sol0mka <legomushroom@gmail.com>
2014-04-15 13:21:10 +0300

    add blue cover

--- js/main.coffee
+++ js/main.coffee
@@ -1,10 +1,48 @@
 class Main
   constructor:->
     @vars()
+    @events()
     @initScroll()
+    @describeSequence()
   vars:->
     @maxScroll = -8000
-    
+    @frameDur = 1000
+    @$cover = $('#js-cover')
+    @$coverPlace = $('#js-cover-place')
+    @$icon1 = $('#js-icon1 .box-icon__content')
+    @$icon2 = $('#js-icon2 .box-icon__content')
+    @$icon3 = $('#js-icon3 .box-icon__content')
+    @$w = $(window)
+
+  events:->
+
+  describeSequence:->
+    start = 1
+    dur = @frameDur
+    @coverTween  = TweenMax.to @$cover, 1,
+      rotationY: 120
+      rotationX: 65
+      x: @$w.width()/6.4
+      y: -400
+      onUpdate: =>
+        progress = @coverTween.progress()
+        if progress > .285
+          @$icon1.css 'z-index': 11
+        else
+          @$icon1.css 'z-index': 1
+
+        if progress > .345
+          @$icon2.css 'z-index': 11
+        else
+          @$icon2.css 'z-index': 1
+
+        if progress > .407
+          @$icon3.css 'z-index': 11
+        else
+          @$icon3.css 'z-index': 1
+
+    @controller.addTween start, @coverTween, dur
+
   initScroll:->
     @scroller = new IScroll '#js-main',
       probeType: 3
--- js/main.js
+++ js/main.js
@@ -4,11 +4,68 @@
   Main = (function() {
     function Main() {
       this.vars();
+      this.events();
       this.initScroll();
+      this.describeSequence();
     }
 
     Main.prototype.vars = function() {
-      return this.maxScroll = -8000;
+      this.maxScroll = -8000;
+      this.frameDur = 1000;
+      this.$cover = $('#js-cover');
+      this.$coverPlace = $('#js-cover-place');
+      this.$icon1 = $('#js-icon1 .box-icon__content');
+      this.$icon2 = $('#js-icon2 .box-icon__content');
+      this.$icon3 = $('#js-icon3 .box-icon__content');
+      return this.$w = $(window);
+    };
+
+    Main.prototype.events = function() {};
+
+    Main.prototype.describeSequence = function() {
+      var dur, start;
+      start = 1;
+      dur = this.frameDur;
+      this.coverTween = TweenMax.to(this.$cover, 1, {
+        rotationY: 120,
+        rotationX: 65,
+        x: this.$w.width() / 6.4,
+        y: -400,
+        onUpdate: (function(_this) {
+          return function() {
+            var progress;
+            progress = _this.coverTween.progress();
+            if (progress > .285) {
+              _this.$icon1.css({
+                'z-index': 11
+              });
+            } else {
+              _this.$icon1.css({
+                'z-index': 1
+              });
+            }
+            if (progress > .345) {
+              _this.$icon2.css({
+                'z-index': 11
+              });
+            } else {
+              _this.$icon2.css({
+                'z-index': 1
+              });
+            }
+            if (progress > .407) {
+              return _this.$icon3.css({
+                'z-index': 11
+              });
+            } else {
+              return _this.$icon3.css({
+                'z-index': 1
+              });
+            }
+          };
+        })(this)
+      });
+      return this.controller.addTween(start, this.coverTween, dur);
     };
 
     Main.prototype.initScroll = function() {
